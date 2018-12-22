284a30a04346ac21be70d4da66368969f45cc817
luqianzhen <403521406@qq.com>
2018-12-22 19:49:09 +0800

    修改测试文件

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+killTomcat()
+{
+    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+    echo "tomcat Id list :$pid"
+    if[ "$pid" = "" ]
+    then
+        echo "no tomcat pid alive"
+    else
+        kill -9 $pid
+    }
+    fi
+}
+
+cd $PROJ_PATH/Guns
+mvn clean install
+
+#停止tomcat
+killTomcat
+
+#删除原有工程
+rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+rm -f $TOMCAT_APP_PATH/webapps/ROOT.jar
+rm -f $TOMCAT_APP_PATH/webapps/guns.jar
+
+# 复制新的工程
+cp $PROJ_PATH/Guns/target/gun.jar $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/webapps
+mv gun.jar ROOT.jar
+
+#启动tomcat
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
+
+
+
